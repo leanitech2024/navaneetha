f534913d1865533b4bbe7e51ab81370b542225bd
Abhijit47 <akarmakar846@gmail.com>
2026-05-12 11:17:41 +0530

    Refactor Footer and Testimonials components for improved structure and styling; add testimonials data to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -108,3 +108,120 @@ export const courses = [
     link: '/courses/pharmacy-nursing',
   },
 ];
+
+export const testimonials = [
+  {
+    id: crypto.randomUUID(),
+    name: 'NEHA G',
+    image:
+      'https://lh3.googleusercontent.com/a/ACg8ocLBokyVaH6tFNLqNpZ26AOsIeQaYTrjO0dUIuE5FxlXN7C1gw=s64-c-rp-mo-br100',
+    ratings: 5,
+    feedback:
+      'I am truly grateful to Navaneetha education trust for their constant support and guidance throughout my MBA admission journey. With their help, I successfully secured my seat at BMSIT College. What I really appreciate is how they not only guided me in choosing the right college but also motivated me to stay confident about my career goals. Their dedication and genuine care for students make them stand out from the rest. Thank you once again for helping me achieve this important milestone in my life. I would highly recommend Navaneetha education trust to any student who is aspiring for education related with the right guidance and support. 🌟',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Venkoba rao',
+    image:
+      'https://lh3.googleusercontent.com/a/ACg8ocL-NfIzZIuVbcnkTqx3Imt7k3014OdtTTUUxzV_NxfNdQIP0g=s64-c-rp-mo-br100',
+    ratings: 5,
+    feedback:
+      'Very good service, Got seat in pes college, Added, One more admission for MBA, Jain college today',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'FARAZ',
+    image:
+      'https://lh3.googleusercontent.com/a-/ALV-UjXqERLVywVT7BVvW2-nvKCM3GDC7kWY3D4gtIQ_GFxKyZh6gxk=s64-c-rp-mo-br100',
+    ratings: 5,
+    feedback:
+      'Raghu sir really helped me during my admission process, his work is highly commendable ❤️',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Shruthi Kulkarni',
+    image:
+      'https://lh3.googleusercontent.com/a/ACg8ocK7moK1Q-ACBlXByU2-TJgy3T6kBkSHGBAPCFIj3an6STpbSg=s64-c-rp-mo-br100',
+    ratings: 5,
+    feedback:
+      'I was referred to this institution  by one of my friends regarding medical admissions. I had the best guidance over the process of admission and the course. I  got into  KIMS through NET. (Navaneetha education trust). I  Highly recommend NET  for the students who are looking for the correct educational guidance and admissions for educational courses.',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Abhishek Vaddhi',
+    image:
+      'https://lh3.googleusercontent.com/a/ACg8ocJTGC56BfR_bo-SJuJKBYI-LvTCZ6hKqqWZiCrg6YV5X48M8g=s64-c-rp-mo-br100',
+    ratings: 5,
+    feedback:
+      "Raghu sir proved the word 'impossible' says I'm possible by getting me an addmission in east west institute of technology in ec branch at a very reasonable cost thank you Navneetha education trust for making my future bright",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Chinmayi BG',
+    image:
+      'https://lh3.googleusercontent.com/a-/ALV-UjWyhB_Xf7celCHH7IVU2Dv9nLBOFvEnm2SAed1WEGYNd4cJRIkU9Q=s64-c-rp-mo-br100',
+    ratings: 5,
+    feedback:
+      'Thank you for providing well managed personal assistance. I would say each one of them are playing their best part to achieve whatever their student wants. Raghu sir Very much trustworthy I must say!🙏🏻💐',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Sandhya Deepa',
+    image:
+      'https://lh3.googleusercontent.com/a/ACg8ocLMTR5XlrqoLutt0op1cWw3Ar7S7ntMXTSoZWYqYPqHIdAXsA=s64-c-rp-mo-br100',
+    ratings: 5,
+    feedback:
+      'I just got admission done, In christ college for BBA, Good service., Navaneetha education trust, Thank you raghu sir',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Vanajakshi Vanajakshi',
+    image:
+      'https://lh3.googleusercontent.com/a/ACg8ocKNVLY8Xe-n7OYQLZVjVA7JMwC1v9WJbwhPoHnb9BFN-HSSiA=s64-c-rp-mo-br100',
+    ratings: 5,
+    feedback:
+      'My son got a good admission in this trust . It was an excellent service and trust worthy.👍',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Royal Tripmaker',
+    image:
+      'https://lh3.googleusercontent.com/a/ACg8ocLb9OgmZRghPRshEWTvmg2UGcSBEsAXWZCfexAztqh3NNRmEg=s64-c-rp-mo-ba2-br100',
+    ratings: 5,
+    feedback:
+      'Very helpful and has amazing knowledge to create successful careers.',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Vidya Kini',
+    image:
+      'https://lh3.googleusercontent.com/a/ACg8ocLsCa37PBP-HC8NWFIrbgAstNGe0OXk10Y6gfiCUrXnaCDqMg=s64-c-rp-mo-br100',
+    ratings: 5,
+    feedback: 'Excellent service and trust worthy!!!',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'K C ROCKS',
+    image:
+      'https://lh3.googleusercontent.com/a/ACg8ocJnTjwAoA2e_OaKGLwnPHasGlaXNve7NbTNu3E-6Sp2296PeQ=s64-c-rp-mo-br100',
+    ratings: 5,
+    feedback: 'Awesome, Best service.., Preeya..',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Vijay Kori',
+    image:
+      'https://lh3.googleusercontent.com/a/ACg8ocJIcyCBfESy4Td-4esCbo2zkHs2pMtuY5GJxoSt0PdtHiWLNA=s64-c-rp-mo-br100',
+    ratings: 5,
+    feedback:
+      'NET is a reputable consulting service that provides reliable assistance. My experience with them exemplifies their timely seeking guidance in the admission process. I would like to express my gratitude to Mr. Raghu chavan for his assistance in helping my son achieve an important milestone in this education. Thank you',
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Rohini Lokesh',
+    image:
+      'https://lh3.googleusercontent.com/a-/ALV-UjVFM8LO_lGZhMD_hbX9NBlEjUCRPaK1KAH1GFmVIfvahA9OtinP=s64-c-rp-mo-br100',
+    ratings: 5,
+    feedback: 'N/A',
+  },
+];
