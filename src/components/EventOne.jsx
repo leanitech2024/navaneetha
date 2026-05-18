8c43a1179453118985e1e41df203c903fc76d3bc
Abhijit47 <akarmakar846@gmail.com>
2026-05-18 12:43:18 +0530

    feat(EventOne): refactor component to use dynamic upcoming events and improve structure

--- src/components/EventOne.jsx
+++ src/components/EventOne.jsx
@@ -1,36 +1,8 @@
-'use client';
-
 import Link from 'next/link';
-// import { useState } from 'react';
-// import ModalVideo from 'react-modal-video';
 import { LazyVideoPlayer } from './video-js-example/lazy';
 
-const EventOne = () => {
-  // const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <section className='event py-120 overflow-hidden'>
-      <div className='container'>
-        <div className='row gy-4'>
-          <div className='col-lg-8 pe-lg-5'>
-            <div className='section-heading style-left'>
-              <div className='flex-align d-inline-flex gap-8 mb-16 wow bounceInDown'>
-                <span className='text-main-600 text-2xl d-flex'>
-                  <i className='ph-bold ph-book-open' />
-                </span>
-                <h5 className='text-main-600 mb-0'>Upcoming Events</h5>
-              </div>
-              <h2 className='mb-24 wow bounceInRight'>
-                Join Our Upcoming Events{' '}
-              </h2>
-              <p className='text-neutral-500 text-line-4 wow bounceInUp'>
-                Join us for a variety of exciting events that cater to your
-                interests and learning needs. Our events are designed to inspire
-                and educate
-              </p>
-            </div>
-            <div className='event-item-wrapper overflow-x-auto scroll-sm scroll-sm-horizontal pb-4'>
-              <div
+/*
+<div
                 className='event-item bg-main-25 rounded-12 p-24 d-flex align-items-center border border-neutral-20 min-width-max-content hover-bg-main-600 hover-border-main-600 hover-text-white transition-1 mb-24'
                 data-aos='fade-up-left'
                 data-aos-duration={200}>
@@ -70,86 +42,108 @@ const EventOne = () => {
                   </Link>
                 </div>
               </div>
-              <div
-                className='event-item bg-main-25 rounded-12 p-24 d-flex align-items-center border border-neutral-20 min-width-max-content hover-bg-main-600 hover-border-main-600 hover-text-white transition-1 mb-24'
-                data-aos='fade-up-left'
-                data-aos-duration={400}>
-                <div className=''>
-                  <h3 className='mb-8 text-main-600'>12</h3>
-                  <span className='text-neutral-500'>Jun, 2024</span>
-                </div>
-                <span className='border border-neutral-40 border-dashed h-72' />
-                <div className=''>
-                  <div className='flex-between gap-16 flex-wrap mb-16'>
-                    <div className='flex-align gap-8'>
-                      <span className='text-neutral-700 text-lg d-flex'>
-                        <i className='ph-bold ph-clock' />
-                      </span>
-                      <span className='text-neutral-700 text-lg fw-normal'>
-                        02:30:am to 04:25:pm
-                      </span>
-                    </div>
-                    <div className='flex-align gap-8'>
-                      <span className='text-neutral-700 text-lg d-flex'>
-                        <i className='ph-bold ph-map-trifold' />
-                      </span>
-                      <span className='text-neutral-700 text-lg fw-normal'>
-                        New York
-                      </span>
-                    </div>
-                  </div>
-                  <h4 className='mt-12 mb-0'>Sports &amp; Health Promotion</h4>
-                </div>
-                <span className='border border-neutral-40 border-dashed h-72' />
-                <div className='flex-shrink-0'>
-                  <Link
-                    href='/about'
-                    className='btn btn-outline-main rounded-pill flex-align d-inline-flex gap-8'>
-                    Join Now
-                    <i className='ph-bold ph-arrow-up-right d-flex text-lg' />
-                  </Link>
-                </div>
+*/
+
+const upcomingEvents = [
+  {
+    id: crypto.randomUUID(),
+    date: '25',
+    month: 'May, 2026',
+    time: '02:30:am to 04:25:pm',
+    location: 'Bengaluru',
+    title: 'Seminar on Comedk counselling and seat allocation',
+    link: '/contact',
+  },
+  {
+    id: crypto.randomUUID(),
+    date: '12',
+    month: 'Jun, 2026',
+    time: '02:30:am to 04:25:pm',
+    location: 'Bengaluru',
+    title: 'Seminar on KCET counselling and allocation Details',
+    link: '/contact',
+  },
+  {
+    id: crypto.randomUUID(),
+    date: '29',
+    month: 'Jul, 2026',
+    time: '02:30:am to 04:25:pm',
+    location: 'Bengaluru',
+    title: 'Seminar on NEET counselling and Seat allocation',
+    link: '/contact',
+  },
+];
+
+const EventOne = () => {
+  return (
+    <section className='event py-120 overflow-hidden'>
+      <div className='container'>
+        <div className='row gy-4'>
+          <div className='col-xl-8 pe-lg-5'>
+            <div className='section-heading style-left'>
+              <div className='flex-align d-inline-flex gap-8 mb-16 wow bounceInDown'>
+                <span className='text-main-600 text-2xl d-flex'>
+                  <i className='ph-bold ph-book-open' />
+                </span>
+                <h5 className='text-main-600 mb-0'>Upcoming Events</h5>
               </div>
-              <div
-                className='event-item bg-main-25 rounded-12 p-24 d-flex align-items-center border border-neutral-20 min-width-max-content hover-bg-main-600 hover-border-main-600 hover-text-white transition-1 mb-0'
-                data-aos='fade-up-left'
-                data-aos-duration={500}>
-                <div className=''>
-                  <h3 className='mb-8 text-main-600'>29</h3>
-                  <span className='text-neutral-500'>Jul, 2024</span>
-                </div>
-                <span className='border border-neutral-40 border-dashed h-72' />
-                <div className=''>
-                  <div className='flex-between gap-16 flex-wrap mb-16'>
-                    <div className='flex-align gap-8'>
-                      <span className='text-neutral-700 text-lg d-flex'>
-                        <i className='ph-bold ph-clock' />
-                      </span>
-                      <span className='text-neutral-700 text-lg fw-normal'>
-                        02:30:am to 04:25:pm
-                      </span>
-                    </div>
-                    <div className='flex-align gap-8'>
-                      <span className='text-neutral-700 text-lg d-flex'>
-                        <i className='ph-bold ph-map-trifold' />
-                      </span>
-                      <span className='text-neutral-700 text-lg fw-normal'>
-                        New York
-                      </span>
-                    </div>
+              <h2 className='mb-24 wow bounceInRight'>
+                Join Our Upcoming Events{' '}
+              </h2>
+              <p className='text-neutral-500 text-line-4 wow bounceInUp'>
+                Join us for a variety of exciting events that cater to your
+                interests and learning needs. Our events are designed to inspire
+                and educate
+              </p>
+            </div>
+            <div className='event-item-wrapper overflow-x-auto scroll-sm scroll-sm-horizontal pb-4'>
+              {upcomingEvents.map((event, index) => (
+                <div
+                  className='event-item bg-main-25 rounded-12 p-24 d-flex align-items-center border border-neutral-20 min-width-max-content hover-bg-main-600 hover-border-main-600 hover-text-white transition-1 mb-24 justify-content-between'
+                  data-aos='fade-up-left'
+                  data-aos-duration={200}
+                  key={event.id}>
+                  <div className=''>
+                    <h3 className='mb-8 text-main-600'>{event.date}</h3>
+                    <span className='text-neutral-500'>{event.month}</span>
                   </div>
-                  <h4 className='mt-12 mb-0'>Career Fair Festivals</h4>
+                  <span className='border border-neutral-40 border-dashed h-72' />
+                  <div className=''>
+                    <div className='flex-between gap-16 flex-wrap mb-16'>
+                      <div className='flex-align gap-8'>
+                        <span className='text-neutral-700 text-lg d-flex'>
+                          <i className='ph-bold ph-clock' />
+                        </span>
+                        <span className='text-neutral-700 text-lg fw-normal'>
+                          {event.time}
+                        </span>
+                      </div>
+                      <div className='flex-align gap-8'>
+                        <span className='text-neutral-700 text-lg d-flex'>
+                          <i className='ph-bold ph-map-trifold' />
+                        </span>
+                        <span className='text-neutral-700 text-lg fw-normal'>
+                          {event.location}
+                        </span>
+                      </div>
+                    </div>
+                    <h4
+                      className='mt-12 mb-0 fs-6 text-break'
+                      style={{ maxWidth: '250px' }}>
+                      {event.title}
+                    </h4>
+                  </div>
+                  <span className='border border-neutral-40 border-dashed h-72' />
+                  <div className='flex-shrink-0'>
+                    <Link
+                      href={event.link}
+                      className='btn btn-outline-main rounded-pill flex-align d-inline-flex gap-8'>
+                      Join Now
+                      <i className='ph-bold ph-arrow-up-right d-flex text-lg' />
+                    </Link>
+                  </div>
                 </div>
-                <span className='border border-neutral-40 border-dashed h-72' />
-                <div className='flex-shrink-0'>
-                  <Link
-                    href='/about'
-                    className='btn btn-outline-main rounded-pill flex-align d-inline-flex gap-8'>
-                    Join Now
-                    <i className='ph-bold ph-arrow-up-right d-flex text-lg' />
-                  </Link>
-                </div>
-              </div>
+              ))}
             </div>
             <Link
               href='/course'
@@ -158,12 +152,12 @@ const EventOne = () => {
               <i className='ph-bold ph-arrow-up-right d-flex text-lg' />
             </Link>
           </div>
-          <div className='col-lg-4'>
+          <div className='col-xl-4'>
             <div className='event-video rounded-16 overflow-hidden position-relative h-100 wow bounceIn'>
               <div className='container wow bounceIn'>
                 <LazyVideoPlayer
                   src={'/videos/highlights.mp4'}
-                  poster={'/services/services-12.png'}
+                  // poster={'/services/services-12.png'}
                   isAspectRatio={false}
                 />
               </div>
