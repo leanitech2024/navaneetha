e02a3b079b8356022b19b3483e6a3bc3c16e5f02
Rajeev K Rao <rajeevraok@gmail.com>
2026-07-04 18:26:15 +0530

    feat: add global animation-disabling styles and create a placeholder animation component

--- src/helper/Animation.jsx
+++ src/helper/Animation.jsx
@@ -1,33 +1,33 @@
 "use client";
 
-import Aos from "aos";
-import { useEffect } from "react";
+// import Aos from "aos";
+// import { useEffect } from "react";
 
 const Animation = () => {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // Import WOW.js dynamically to ensure it runs only on the client side
-      import("wowjs").then((WOW) => {
-        const wow = new WOW.WOW({
-          boxClass: "wow", // Class name for animations
-          animateClass: "animate__animated", // Animation CSS class
-          offset: 0, // Trigger animations when elements are in view
-          mobile: true, // Enable animations on mobile
-          live: true, // Check for dynamically added elements
-        });
-        wow.init();
-      });
-    }
-
-    // Initialize AOS
-    Aos.init({
-      offset: 0,
-      easing: "ease",
-      once: true,
-      duration: 2000,
-    });
-    Aos.refresh();
-  }, []);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     // Import WOW.js dynamically to ensure it runs only on the client side
+  //     import("wowjs").then((WOW) => {
+  //       const wow = new WOW.WOW({
+  //         boxClass: "wow", // Class name for animations
+  //         animateClass: "animate__animated", // Animation CSS class
+  //         offset: 0, // Trigger animations when elements are in view
+  //         mobile: true, // Enable animations on mobile
+  //         live: true, // Check for dynamically added elements
+  //       });
+  //       wow.init();
+  //     });
+  //   }
+  // 
+  //   // Initialize AOS
+  //   Aos.init({
+  //     offset: 0,
+  //     easing: "ease",
+  //     once: true,
+  //     duration: 2000,
+  //   });
+  //   Aos.refresh();
+  // }, []);
 
   return null;
 };
